2be060bb3daf79381441a0fc9e2d72767e19b139
SamuelMTeixeira <samuelmolendolff@hotmail.com>
2022-07-27 00:39:07 -0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 import './assets/css/App.css'
 
@@ -16,6 +17,7 @@ function App() {
         <Projects />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '../assets/css/Footer.css'
+
+import { Container, Row, Col } from 'react-bootstrap'
+
+// ICONES
+import {
+    GithubFilled,
+    LinkedinFilled,
+    InstagramFilled,
+} from '@ant-design/icons';
+
+const Footer = () => {
+    return (
+        <footer>
+            <Container>
+                <Row className='d-flex align-items-center'>
+                    <Col sm={6} className='text-center text-sm-start'>
+                    <p>Copyright 2022. All Rights Reserved</p>
+                    </Col>
+
+                    <Col sm={6} className='text-center text-sm-end'>
+                        <div className='social-icons px-3'>
+                            <a href='https://www.linkedin.com/in/SamuelMTeixeira/' > <LinkedinFilled />  </a>
+                            <a href='https://github.com/SamuelMTeixeira' > <GithubFilled />  </a>
+                            <a href='https://www.instagram.com/this.samuelteixeira/' > <InstagramFilled /> </a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    )
+}
+
+export default Footer
