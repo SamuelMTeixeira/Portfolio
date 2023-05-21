9b3b621d2271bf6e8541c29098b4e1c6bede5007
Samuel Molendolff Teixeira <samuelmolendolff@hotmail.com>
2023-05-21 00:05:48 -0300

    refactor: modify and add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,33 @@ import jsonLd from '@/data/metadata.json'
 export const metadata = {
   title: "Samuel Molendolff Teixeira",
   description: "A Software Developer's Portofio",
+  authors: { name: "Samuel Molendolff Teixeira", url: "https://www.linkedin.com/in/samuelmteixeira/" },
+  keywords: [
+    "Portfolio",
+    "Developer",
+    "Software Developer",
+    "Front-End Developer",
+    "Back-End Developer",
+    "Software Engineer",
+    "Desenvolvedor",
+    "Front-End",
+    "Back-End",
+    "Engenheiro de software"
+  ],
+  alternates: {
+    canonical: "/"
+  },
+  openGraph: {
+    images: {
+      url: "/logo.png",
+      alt: "Yellow letter S, used as the representative logo of the website",
+      type: "image/png"
+    },
+    title: "Samuel Molendolff Teixeira",
+    type: "website",
+    description: "Check out my software development portfolio, where I share my projects and experiences in the area.",
+    url: "https://samuelmteixeira.dev",
+  }
 }
 
 export default function RootLayout({
