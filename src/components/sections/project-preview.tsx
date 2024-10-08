2727299152b2a60022d58b06ceb27cf1b0c9c1fb
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-10-07 23:14:40 -0300

    feat: add useProject hook for image previews

--- src/components/sections/project-preview.tsx
+++ src/components/sections/project-preview.tsx
@@ -1,66 +1,33 @@
 'use client'
 
-import preview from '@/assets/img/project-previews/preview.png'
-import preview2 from '@/assets/img/project-previews/preview-2.png'
 import Image from 'next/image'
 import { motion, useTransform, useScroll } from 'framer-motion'
 import { useRef } from 'react'
+import useProject from '@/hooks/useProject'
 
 export default function ProjectPreview() {
   const containerRef = useRef(null)
   const { scrollYProgress } = useScroll({ target: containerRef })
   const x = useTransform(scrollYProgress, [0, 1], ['0%', '-30%'])
 
+  const { data } = useProject()
+
   return (
     <div className="overflow-x-hidden mt-10" ref={containerRef}>
       <motion.div className="flex items-center flex-row gap-5" style={{ x }}>
-        <Image
-          src={preview}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
 
-        <Image
-          src={preview2}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
-
-        <Image
-          src={preview}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
-
-        <Image
-          src={preview2}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
-
-        <Image
-          src={preview}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
-
-        <Image
-          src={preview2}
-          className="rounded-md"
-          alt="Project preview"
-          width={400}
-          height={400}
-        />
+        {
+          data?.map(({imageUrl}, index) => (
+            <Image
+              key={index}
+              src={imageUrl}
+              className="rounded-md w-[370px] h-[256px]"
+              alt="Project preview"
+              width={470}
+              height={356}
+            />
+          ))
+        }
       </motion.div>
     </div>
   )
