f5000229e0eafc8c2b95a8c7874324bf71a415e0
Samuel Molendolff Teixeira <contact@samuelmteixeira.dev>
2024-10-09 21:11:46 -0300

    fix: images returning error 402

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
